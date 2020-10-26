b9f099da855f605be7530ff65b60f39fd7b560ef
abdulmumeen <abdulmumeen15500@gmail.com>
2020-10-26 15:09:06 +0100

    add search feature

--- routes/api/post-route.js
+++ routes/api/post-route.js
@@ -7,6 +7,7 @@ const {
   viewPosts,
   editPost,
   deletePost,
+  search,
 } = require("./post-controller");
 const { ensureAuth } = require("../../middlewares/auth");
 const apiRouter = require("express").Router();
@@ -15,6 +16,7 @@ apiRouter.get("/posts", ensureAuth, viewPosts);
 apiRouter.get("/posts/create", ensureAuth, (req, res) => {
   res.render("create-post");
 });
+apiRouter.get("/posts/search", search);
 apiRouter.post("/posts/create", createPost);
 apiRouter.get("/posts/:id", ensureAuth, viewPost);
 apiRouter.get("/posts/edit/:id", ensureAuth, viewEdit);
